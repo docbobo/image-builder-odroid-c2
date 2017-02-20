5dcc79710564e00812d61703ece45da5e75f98d8
Boris Pruessmann <bpruessm@adobe.com>
2017-02-20 12:25:32 +0100

    Continued Odroid C2 work

--- builder/chroot-script.sh
+++ builder/chroot-script.sh
@@ -1,51 +1,161 @@
 #!/bin/bash
 set -ex
 
+KEYSERVER="ha.pool.sks-keyservers.net"
+
+function clean_print(){
+  local fingerprint="${2}"
+  local func="${1}"
+
+  nospaces=${fingerprint//[:space:]/}
+  tolowercase=${nospaces,,}
+  KEYID_long=${tolowercase:(-16)}
+  KEYID_short=${tolowercase:(-8)}
+  if [[ "${func}" == "fpr" ]]; then
+    echo "${tolowercase}"
+  elif [[ "${func}" == "long" ]]; then
+    echo "${KEYID_long}"
+  elif [[ "${func}" == "short" ]]; then
+    echo "${KEYID_short}"
+  elif [[ "${func}" == "print" ]]; then
+    if [[ "${fingerprint}" != "${nospaces}" ]]; then
+      printf "%-10s %50s\n" fpr: "${fingerprint}"
+    fi
+    # if [[ "${nospaces}" != "${tolowercase}" ]]; then
+    #   printf "%-10s %50s\n" nospaces: $nospaces
+    # fi
+    if [[ "${tolowercase}" != "${KEYID_long}" ]]; then
+      printf "%-10s %50s\n" lower: "${tolowercase}"
+    fi
+    printf "%-10s %50s\n" long: "${KEYID_long}"
+    printf "%-10s %50s\n" short: "${KEYID_short}"
+    echo ""
+  else
+    echo "usage: function {print|fpr|long|short} GPGKEY"
+  fi
+}
+
+
+function get_gpg(){
+  GPG_KEY="${1}"
+  KEY_URL="${2}"
+
+  clean_print print "${GPG_KEY}"
+  GPG_KEY=$(clean_print fpr "${GPG_KEY}")
+
+  if [[ "${KEY_URL}" =~ ^https?://* ]]; then
+    echo "loading key from url"
+    KEY_FILE=temp.gpg.key
+    wget -q -O "${KEY_FILE}" "${KEY_URL}"
+  elif [[ -z "${KEY_URL}" ]]; then
+    echo "no source given try to load from key server"
+#    gpg --keyserver "${KEYSERVER}" --recv-keys "${GPG_KEY}"
+    apt-key adv --keyserver "${KEYSERVER}" --recv-keys "${GPG_KEY}"
+    return $?
+  else
+    echo "keyfile given"
+    KEY_FILE="${KEY_URL}"
+  fi
+
+  FINGERPRINT_OF_FILE=$(gpg --with-fingerprint --with-colons "${KEY_FILE}" | grep fpr | rev |cut -d: -f2 | rev)
+
+  if [[ ${#GPG_KEY} -eq 16 ]]; then
+    echo "compare long keyid"
+    CHECK=$(clean_print long "${FINGERPRINT_OF_FILE}")
+  elif [[ ${#GPG_KEY} -eq 8 ]]; then
+    echo "compare short keyid"
+    CHECK=$(clean_print short "${FINGERPRINT_OF_FILE}")
+  else
+    echo "compare fingerprint"
+    CHECK=$(clean_print fpr "${FINGERPRINT_OF_FILE}")
+  fi
+
+  if [[ "${GPG_KEY}" == "${CHECK}" ]]; then
+    echo "key OK add to apt"
+    apt-key add "${KEY_FILE}"
+    rm -f "${KEY_FILE}"
+    return 0
+  else
+    echo "key invalid"
+    exit 1
+  fi
+}
+
+## examples:
+# clean_print {print|fpr|long|short} {GPGKEYID|FINGERPRINT}
+# get_gpg {GPGKEYID|FINGERPRINT} [URL|FILE]
+
 # device specific settings
 HYPRIOT_DEVICE="ODROID C2"
 
 # set up /etc/resolv.conf
-echo "nameserver 8.8.8.8" > /etc/resolv.conf
+DEST=$(readlink -m /etc/resolv.conf)
+export DEST
+mkdir -p "$(dirname "${DEST}")"
+echo "nameserver 8.8.8.8" > "${DEST}"
 
 # set up ODROID repository
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys AB19BAC9
+ODROID_KEY_ID=AB19BAC9
+get_gpg $ODROID_KEY_ID
 echo "deb http://deb.odroid.in/c2/ xenial main" > /etc/apt/sources.list.d/odroid.list
 
-# # set up Hypriot Schatzkiste repository
-# wget -q https://packagecloud.io/gpg.key -O - | apt-key add -
-# echo 'deb https://packagecloud.io/Hypriot/Schatzkiste/debian/ wheezy main' > /etc/apt/sources.list.d/hypriot.list
+# set up hypriot arm repository for odroid packages
+PACKAGECLOUD_FPR=418A7F2FB0E1E6E7EABF6FE8C2E73424D59097AB
+PACKAGECLOUD_KEY_URL=https://packagecloud.io/gpg.key
+get_gpg "${PACKAGECLOUD_FPR}" "${PACKAGECLOUD_KEY_URL}"
+
+# set up hypriot schatzkiste repository for generic packages
+echo 'deb https://packagecloud.io/Hypriot/Schatzkiste/debian/ jessie main' >> /etc/apt/sources.list.d/hypriot.list
+
+# set up armbian repository
+ARMBIAN_KEY_ID=0x93D6889F9F0E78D5
+get_gpg "${ARMBIAN_KEY_ID}"
+echo "deb http://apt.armbian.com jessie main utils jessie-desktop" | tee /etc/apt/sources.list.d/armbian.list
+
+# add armhf as additional architecure (see below)
+dpkg --add-architecture armhf
 
 # update all apt repository lists
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
+apt-get upgrade -y
 
-# ---install Docker tools---
+# create /etc/fstab
+echo "
+proc /proc proc defaults 0 0
+/dev/mmcblk0p1 /boot vfat defaults 0 0
+/dev/mmcblk0p2 / ext4 defaults,noatime 0 1
+" > /etc/fstab
 
-# # install Hypriot packages for using Docker
-# apt-get install -y \
-#   "docker-hypriot=${DOCKER_ENGINE_VERSION}" \
-#   "docker-compose=${DOCKER_COMPOSE_VERSION}" \
-#   "docker-machine=${DOCKER_MACHINE_VERSION}"
+# as the Odroid C2 does not have a hardware clock we need a fake one
+apt-get install -y \
+  fake-hwclock
 
-# #FIXME: should be handled in .deb package
-# # setup Docker default configuration for ODROID C2
-# rm -f /etc/init.d/docker # we're using a pure systemd init, remove sysvinit script
-# rm -f /etc/default/docker
-# # --get upstream config
-# wget -q -O /etc/default/docker https://github.com/docker/docker/raw/master/contrib/init/sysvinit-debian/docker.default
-# # --enable aufs by default
-# sed -i "/#DOCKER_OPTS/a \
-# DOCKER_OPTS=\"--storage-driver=aufs -D\"" /etc/default/docker
+# install docker-tools
+apt-get -y install lxc aufs-tools cgroupfs-mount cgroup-bin apparmor libltdl7
 
-# #FIXME: should be handled in .deb package
-# # enable Docker systemd service
-# systemctl enable docker
+# install Hypriot packages for using Docker
+apt-get install -y \
+    "libc6:armhf" \
+    "zlib1g:armhf" \
+    "device-init:armhf=${DEVICE_INIT_VERSION}" \
+    "docker-compose:armhf=${DOCKER_COMPOSE_VERSION}" \
+    "docker-machine:armhf=${DOCKER_MACHINE_VERSION}"
+
+DOCKER_DEB=`mktemp`
+wget -q -O $DOCKER_DEB $DOCKER_DEB_URL
+dpkg -i $DOCKER_DEB
+rm $DOCKER_DEB
 
 # install ODROID C2 u-boot and kernel
 apt-get install -y u-boot-tools initramfs-tools
 apt-get install -y linux-image-c2
-ls -al /boot/*
+
+# cleanup APT cache and lists
+apt-get clean
+rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
 # set device label and version number
 echo "HYPRIOT_DEVICE=\"$HYPRIOT_DEVICE\"" >> /etc/os-release
 echo "HYPRIOT_IMAGE_VERSION=\"$HYPRIOT_IMAGE_VERSION\"" >> /etc/os-release
+cp /etc/os-release /boot/os-release
